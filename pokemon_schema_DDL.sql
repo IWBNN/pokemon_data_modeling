4d7caaf54e5a425a5a0a282914b74d70f5916b19
IWBNN <deangeon@naver.com>
2024-02-27 17:44:58 +0900

    update .idea gitignore

--- pokemon_schema_DDL.sql
+++ pokemon_schema_DDL.sql
@@ -1,2 +1,36 @@
-create database pokemon_game;
+create table poke_dex -- 포켓몬 도감 table 설정
+(
+    id              int auto_increment primary key,
+    elemental varchar(20),
+    monsterName     varchar(20) not null,
+    maxHp           int         not null,
+    skill1Name      varchar(20) not null,
+    skill1Dmg       int         not null,
+    skill2Name      varchar(20),
+    skill2Dmg       int,
+    beforeEvolvedId int
+);
 
+select * from poke_dex;
+
+create table pokemon_trainer ( -- 트레이너 table 작성
+    id int primary key auto_increment,
+    name varchar(20) not null,
+    capturedPokemon json
+);
+
+
+select * from pokemon_trainer;
+
+create table pokemon -- 포켓몬 table 설정
+(
+    id              int auto_increment primary key,
+    monsterName     varchar(20) not null,
+    elemental       varchar(20),
+    maxHp           int         not null,
+    hp              int not null,
+    skill1Name      varchar(20) not null,
+    skill1Dmg       int         not null,
+    skill2Name      varchar(20),
+    skill2Dmg       int
+);
--- program_logic_sample_DML.sql
+++ program_logic_sample_DML.sql
@@ -1,2 +1,30 @@
 -- 아주 기본적인 데이터 편집 쿼리만 작성
 -- 연습용 샘플
+
+-- 특정 트레이너가 보유한 모든 포켓몬 출력
+SELECT capturedPokemon
+FROM pokemon_trainer
+WHERE name = 'Trainer1';
+
+-- 특정 속성 ELEMENTAL의 포켓몬을 모두 출력하는 쿼리 작성
+SELECT monsterName
+from poke_dex
+where elemental = '물';
+
+-- 특정 트레이너가 보유한 포켓몬들의 속성에 대한 통계를 도출하는 Aggregation 쿼리 작성
+-- 특정 트레이너가 보유한 포켓몬들의 각 속성에 대한 수
+SELECT
+    pd.elemental,
+    COUNT(*) AS count
+FROM
+    poke_dex pd
+        JOIN
+    pokemon_trainer pt ON JSON_UNQUOTE(JSON_EXTRACT(pt.capturedPokemon, '$.*')) = pd.monsterName
+WHERE
+    pt.name = 'Trainer1'
+GROUP BY
+    pd.elemental;
+
+
+select * from pokemon_trainer where name = 'Trainer1';
+
